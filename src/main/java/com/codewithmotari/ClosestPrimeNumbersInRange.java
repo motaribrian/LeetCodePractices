f8cde45bbe477f6a0869212d171bbcd720f284be
sharon imali <imalisharon68@gmail.com>
2025-03-07 13:03:52 +0300

    got rid of null pointer excemptions

--- src/main/java/com/codewithmotari/ClosestPrimeNumbersInRange.java
+++ src/main/java/com/codewithmotari/ClosestPrimeNumbersInRange.java
@@ -21,22 +21,41 @@ Since 11 is smaller than 17, we return the first pair.
 
 package com.codewithmotari;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class ClosestPrimeNumbersInRange {
-    public int[] closestPrimes(int left, int right) {
-        int num1=-1 , num2=-1;
-        Set<Integer> prime=new TreeSet<>();
-        //add prime numbers to treeset
-        for (int i=left;i<right;i++){
-            if(isprime(i)){
-                prime.add(i);
-            }
-        }
-        return new int[2];
+    public static void main(String[] args) {
+        ClosestPrimeNumbersInRange obj = new ClosestPrimeNumbersInRange();
+        System.out.println(obj.closestPrimes(10,19)[0]+obj.closestPrimes(10,19)[1]);
     }
-    public boolean isprime(int n){
+     public Set<Integer> getALlPrimesInBetween(int left ,int right){
+         System.out.println("calling getAllPrimesInBetween");
+         if(isprime(left) && isprime(right) && left<=right){
+             Set<Integer> primes=new TreeSet<>();
+             //add prime numbers to treeset
+             for (int i=left;i<right;i++){
+                 if(isprime(i)){
+                     primes.add(i);
+                 }
+             }
+             return primes;
+         }
+         //if there"s no prime numbers just return an empty set
+         return new TreeSet<>();
+     }
+
+    public  int[] closestPrimes(int left,int right) {
+        //executes if there are prime numbers in between
+        if(!this.getALlPrimesInBetween(left, right).isEmpty()) {
+            return new int[]{left,right};
+        }
+
+//        executes if no prime numbers are present inbetween or specified rules are broken
+        return new int[]{0,0};
+    }
+    public static boolean isprime(int n){
         if(n<1){
             return false;
         }
